7f0b3f4bb6b61c1a1070616121aaf3a74833e844
Nurzhan <nurzhan.aytimov@gmail.com>
2025-03-23 16:39:25 +0500

    feat(): add email service

---
+++ QuizIdentity.Infrastructure.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QuizIdentity.Infrastructure.Shared.Services.Implementations;
+using QuizIdentity.Infrastructure.Shared.Services.Interfaces;
+
+namespace QuizIdentity.Infrastructure.Shared.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddSharedInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IEmailService, EmailService>();
+    }
+}
---
+++ QuizIdentity.Infrastructure.Shared/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace QuizIdentity.Infrastructure.Shared.Services.Interfaces;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string emailFrom, string body, string subject);
+}
